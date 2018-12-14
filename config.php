8da932dda2d3d8a7ffb8a97057228f8497e6766f
Jeison Cardona Gómez <jeison.cardona@correounivalle.edu.co>
2018-12-14 09:23:47 -0500

    config

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php  // Moodle configuration file
+
+unset($CFG);
+global $CFG;
+$CFG = new stdClass();
+
+$CFG->dbtype    = 'pgsql';
+$CFG->dblibrary = 'native';
+$CFG->dbhost    = 'localhost';
+$CFG->dbname    = 'moodle35';
+$CFG->dbuser    = 'moodle35';
+$CFG->dbpass    = 'moodle35';
+$CFG->prefix    = 'mdl_';
+$CFG->dboptions = array (
+  'dbpersist' => 0,
+  'dbport' => '',
+  'dbsocket' => '',
+);
+
+$CFG->wwwroot   = 'http://127.0.0.1/moodle35';
+$CFG->dataroot  = '/var/www/moodledata34';
+$CFG->admin     = 'admin';
+
+$CFG->directorypermissions = 0777;
+
+require_once(__DIR__ . '/lib/setup.php');
+
+// There is no php closing tag in this file,
+// it is intentional because it prevents trailing whitespace problems!
